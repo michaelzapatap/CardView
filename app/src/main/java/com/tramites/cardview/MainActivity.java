b9692969b89604a1935c2948d5d714465123eb63
desarrollofasemovil <desarrollofasemovil@hotmail.com>
2021-06-25 15:09:05 -0500

    Initial commit

---
+++ app/src/main/java/com/tramites/cardview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tramites.cardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView)findViewById(R.id.textViewTitle);
+        textView.setText("Bienvenido a Solintech Sistemas");
+    }
+}
